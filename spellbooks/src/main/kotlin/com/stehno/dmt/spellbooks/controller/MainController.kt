cceaacfb7ab02711bdf9571d5a232411119bed8a
Christopher J. Stehno <chris.stehno@gmail.com>
2018-08-10 14:58:38 -0500

    simple starting point for spell importing

--- spellbooks/src/main/kotlin/com/stehno/dmt/spellbooks/controller/MainController.kt
+++ spellbooks/src/main/kotlin/com/stehno/dmt/spellbooks/controller/MainController.kt
@@ -1,8 +1,27 @@
 package com.stehno.dmt.spellbooks.controller
 
+import com.stehno.dmt.spellbooks.dsl.SpellbookLoader
+import javafx.event.ActionEvent
+import javafx.stage.FileChooser
+
 class MainController {
 
-    fun initialize(){
+    fun initialize() {
 
+    }
+
+    fun onImport(event: ActionEvent) {
+        val selectedFile = FileChooser().apply {
+            title = "Import Spells"
+            selectedExtensionFilter = FileChooser.ExtensionFilter("Spellbooks", mutableListOf("*.tome", "*.codex"))
+        }.showOpenDialog(null)
+
+        // TODO: run this on a separate thread to allow UI to continue
+
+        if( selectedFile.extension in listOf("tome", "codex")){
+            val spellbook = SpellbookLoader.load(selectedFile)
+            // TODO: a progress screen with scrolling list of spells as loaded...
+            println("Loaded ${spellbook.spells.size} spells from ${spellbook.title}.")
+        }
     }
 }
