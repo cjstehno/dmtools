37db0955aa3d214234d5b495300e3d95924bdf3d
stehnoc <chris.stehno@netscout.com>
2019-02-03 15:34:42 -0600

    almost done with first pass

--- treasure/src/main.rs
+++ treasure/src/main.rs
@@ -1,5 +1,6 @@
 extern crate csv;
 extern crate rand;
+extern crate regex;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
@@ -8,15 +9,17 @@ use std::env;
 use std::process;
 use std::vec::Vec;
 
+use regex::Regex;
+
 // FIXME: better error handling
 
 #[derive(Debug)]
 struct Treasure {
-    cp: u32,
-    sp: u32,
-    ep: u32,
-    gp: u32,
-    pp: u32,//,
+    cp: u16,
+    sp: u16,
+    ep: u16,
+    gp: u16,
+    pp: u16,//,
 //    gems: u32,
 //    gemValue: u32,
 //    art: u32,
@@ -32,6 +35,14 @@ struct IndividualTreasure {
     ep: String,
     gp: String,
     pp: String,
+}
+
+#[derive(Debug)]
+struct DieRoll {
+    count: u16,
+    d: u16,
+    modifier: u16,
+    multiplier: u16,
 }
 
 // TODO: --individual|--hoard(-i|-h) --cr=# --count=#[1]
@@ -60,7 +71,7 @@ fn individual_treasure(cr: u8) -> Treasure {
                 sp: roll_dice(&row.sp),
                 ep: roll_dice(&row.ep),
                 gp: roll_dice(&row.gp),
-                pp: roll_dice(&row.pp)
+                pp: roll_dice(&row.pp),
             }
         }
         None => Treasure { cp: 0, sp: 0, ep: 0, gp: 0, pp: 0 }
@@ -103,10 +114,45 @@ fn load_individual_table(path: &str) -> Vec<IndividualTreasure> {
     table_items
 }
 
-fn roll_dice(dice: &str) -> u32 {
+fn roll_dice(dice: &str) -> u16 {
     if dice == "-" {
-        return 0
+        return 0;
     } else {
-        return 1 // FIXME: parse and gen the random roll
+        let rx = Regex::new("([0-9]*)d([0-9]*)[+]?([0-9]*)[x]?([0-9]*)").unwrap();
+        println!("Matches: {}", rx.is_match(dice));
+
+        let groups = rx.captures(dice).unwrap();
+        let die_roll = DieRoll {
+            count: str_to_num(groups.get(1).unwrap().as_str(), 1),
+            d: str_to_num(groups.get(2).unwrap().as_str(), 0),
+            modifier: str_to_num(groups.get(3).unwrap().as_str(), 0),
+            multiplier: str_to_num(groups.get(4).unwrap().as_str(), 1),
+        };
+        println!("Die: {:?}", die_roll);
+
+        return roll(die_roll);
     }
 }
+
+// FIXME: refactor some of this into DieRoll impl
+fn str_to_num(value: &str, default_value: u16) -> u16 {
+    if value.is_empty() {
+        default_value
+    } else {
+        value.parse().unwrap()
+    }
+}
+
+fn roll(dice: DieRoll) -> u16 {
+    let mut rolls = 0;
+
+    for r in 0..dice.count {
+        let rolled = rand::random::<u16>() % dice.d;
+        println!("Rolled ({}): {}", dice.d, rolled);
+        rolls += rolled;
+    }
+
+    (rolls + dice.modifier) * dice.multiplier
+}
+
+the random % is returning 0
