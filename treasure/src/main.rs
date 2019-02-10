d8537dadc45adde9417f3c48b35abcd4d77db4fb
stehnoc <chris.stehno@netscout.com>
2019-02-10 14:40:44 -0600

    refactoring

--- treasure/src/main.rs
+++ treasure/src/main.rs
@@ -26,38 +26,31 @@ fn main() {
         .arg(Arg::with_name("verbose").long("verbose").short("v").multiple(true).help("Turns on verbose operation logging information."))
         .arg(Arg::with_name("hoard").long("hoard").help("Generates hoard treasure."))
         .arg(Arg::with_name("cr").long("cr").short("c").value_name("CHALLENGE-RATING").help("Specifies the Challenge Rating.").required(true).takes_value(true))
-        .arg(Arg::with_name("rolls").long("rolls").short("r").value_name("# ROLLS").help("Number of treasure rolls to generate.").takes_value(true))
         .get_matches();
 
     let cr: u8 = matches.value_of("cr").unwrap().parse().unwrap();
-    let rolls: u8 = matches.value_of("rolls").unwrap_or("1").parse().unwrap();
     let verbose_count = matches.occurrences_of("verbose");
     let generate_hoard: bool = matches.occurrences_of("hoard") > 0;
 
-    if verbose_count > 0 {
-        enable_verbose(verbose_count).unwrap();
-    }
-
-    let treasure_category = if generate_hoard { "Hoard" } else { "Individual" };
-
-    debug!("Rolling {} {} CR-{} treasure(s).", rolls, treasure_category, cr);
+    enable_verbose(verbose_count);
 
     let treasure = match generate_hoard {
         true => TreasureDefinition::roll_treasure("hoard", cr),
         false => TreasureDefinition::roll_treasure("individual", cr)
     };
 
-    println!("CR-{} {} Treasure:\n{}", cr, treasure_category, treasure);
+    println!("CR-{} {} Treasure:\n{}", cr, if generate_hoard { "Hoard" } else { "Individual" }, treasure);
 }
 
-fn enable_verbose(verbose_count: u64) -> Result<(), fern::InitError> {
-    fern::Dispatch::new()
-        .format(|out, message, record| {
-            out.finish(format_args!("[{}] {}", record.level(), message))
-        })
-        .level(if verbose_count == 1 { log::LevelFilter::Debug } else  { log::LevelFilter::Trace })
-        .chain(std::io::stdout())
-        .apply()?;
-
-    Ok(())
+fn enable_verbose(verbose_count: u64) {
+    if verbose_count > 0 {
+        fern::Dispatch::new()
+            .format(|out, message, record| {
+                out.finish(format_args!("[{}] {}", record.level(), message))
+            })
+            .level(if verbose_count == 1 { log::LevelFilter::Debug } else { log::LevelFilter::Trace })
+            .chain(std::io::stdout())
+            .apply()
+            .unwrap();
+    }
 }
